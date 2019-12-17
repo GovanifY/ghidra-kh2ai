d12972a0d11878cbb2b5649cfd36fd7aec17e9d5
Gauvain "GovanifY" Roussel-Tarbouriech <gauvain@govanify.com>
2019-12-17 13:04:30 +0100

    initial commit; started earlier today

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd data/languages
+/home/govanify/Documents/projects/programming/hacking/tools/ghidra/Ghidra/Features/Decompiler/os/linux64/sleigh -c -u -a
+cp -rf ../../   /home/govanify/Documents/projects/programming/hacking/tools/ghidra/Ghidra/Processors/kh2a
+cd ../../
